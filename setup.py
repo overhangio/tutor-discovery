9090614180786ed87ec26ddbbea54c285d2ff220
Muhammad Faraz Maqsood <faraz.maqsood@arbisoft.com>
2024-11-14 21:09:52 +0500

    chore: remove Python 3.8 references (#91)
    - drop support for python 3.8 and set Python 3.9 as the minimum supported python version.
    
    Co-authored-by: Muhammad Faraz  Maqsood <faraz.maqsood@192.168.10.36>

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     include_package_data=True,
     install_requires=["tutor>=18.0.0,<19.0.0"],
     extras_require={"dev": "tutor[dev]>=18.0.0,<19.0.0"},
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     entry_points={"tutor.plugin.v1": ["discovery = tutordiscovery.plugin"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -44,7 +44,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
